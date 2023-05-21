d33b8c58f81831deec26fadb5c376f97fa92335a
oMotaS <vimotas@outlook.com>
2023-05-21 20:08:52 -0300

    fet: added validation

--- js/cp3.js
+++ js/cp3.js
@@ -1,16 +1,39 @@
 const validate = (e) => {
   const value = e.target.value
   const min = 5
-  const max = e.target.type === 'password' && 8
+  const max = e.target.type === 'password' ? 8 : null
   const isEmail = e.target.id === 'input-email'
-  const allowEmpty = false
+  const isConfirmPass = e.target.id === 'input-confirm'
 
-  if (value < min) {
-    console.log('Este campo deve conter no minino 5 caracteres')
-  } else if (max) {
+  const inputContainer = document.querySelector(`#${e.target.name}-row .error-label`)
+
+  if (value.length < min) {
+    inputContainer.innerHTML = `*O campo ${e.target.name} deve conter no minimo 5`
+  } else if (max && value.length > max) {
+    inputContainer.innerHTML = '*A senha deve conter no maximo 8 caracteres'
+  } else if (isEmail && !value.includes('@')) {
+    inputContainer.innerHTML = 'O email deve conter @'
+  } else if (value.length < 1) {
+    inputContainer.innerHTML = `O campo ${e.target.name} é obrigatório`
+  } else if (isConfirmPass) {
+    const passInput = document.getElementById('input-pass')
+    inputContainer.innerHTML = ''
+    if (passInput.value != value) {
+      inputContainer.innerHTML = '*Senhas incompatíveis'
+    }
+  } else {
+    inputContainer.innerHTML = ''
   }
 }
 
-document.querySelectorAll('input[name="create-account"]').forEach((input) => {
+const alertError = (e) => {
+  const errorLabels = [...document.querySelectorAll('.error-label')]
+  errorLabels.map((label) => label.innerHTML)
+  //   console.log(errorLabels)
+  //   const errors = errorLabels.map((label) => label.innerHTML)
+  //   console.log(errors)
+}
+document.getElementById('btn-new-account').addEventListener('click', alertError)
+document.querySelectorAll('.text-field').forEach((input) => {
   input.addEventListener('input', validate)
 })
