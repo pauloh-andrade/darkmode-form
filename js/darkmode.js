44b6de0ef3b4edf0fc37590fa0d343770bbf8834
oMotaS <vimotas@outlook.com>
2023-05-21 18:01:55 -0300

    feat: finish darkmode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,28 +1,29 @@
 'use strict'
-const toggleMode = () => {
-  const root = document.documentElement
+const changeImages = (isDarkMode) => {
   const trail = document.querySelector('.trail')
   const footerLogo = document.querySelector('.footer-logo')
   const mode = document.querySelector('#change-mode-button img')
-  const currentColor = getComputedStyle(root).getPropertyValue('--primary-background')
 
-  if (currentColor === '#282a37') {
+  if (isDarkMode) {
     trail.src = './img/trail-white.png'
     footerLogo.src = './img/footer-logo-b.png'
-    mode.src = './img/mode-dark.png'
-
-    root.style.setProperty('--primary-background', '#fff')
-    root.style.setProperty('--primary-background-grad', '#FFFFFFCC')
-    root.style.setProperty('--secundary-background', '#F0F0F0')
-    root.style.setProperty('--primary-font', '#3d3d3d')
-    root.style.setProperty('--secundary-font', '#737C88')
-    root.style.setProperty('--primary-border', '#737C88')
+    mode.src = './img/mode-dark.svg'
   } else {
     trail.src = './img/trail-large.png'
     footerLogo.src = './img/footer-logo.png'
     mode.src = './img/mode-light.svg'
+  }
+}
+const toggleMode = () => {
+  const root = document.documentElement
+  const isDarkMode = !root.classList.contains('light')
 
-    root.removeAttribute('style')
+  if (isDarkMode) {
+    root.classList.toggle('light')
+    changeImages(isDarkMode)
+  } else {
+    root.classList.remove('light')
+    changeImages(isDarkMode)
   }
 }
 
